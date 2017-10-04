fef875afd8cf8d59e27f95d46c2dbd348cea7357
Unknown <denys.fiialko@gmail.com>
2017-10-04 15:32:40 -0500

    Patient Component

--- src/app/home/patients/patientModel/patient.model.ts
+++ src/app/home/patients/patientModel/patient.model.ts
@@ -1,14 +1,16 @@
-export class Patient{
+export class Patient {
 
-    public email:string;
-    public name:string;
-    public birth:string;
-    public phone:string;
+    public email: string;
+    public name: string;
+    public birth: string;
+    public phone: string;
+    public gender: string;
 
-    constructor(email:string, name:string, birth:string, phone:string){
+    constructor(email: string, name: string, birth: string, phone: string, gender: string) {
         this.email = email;
         this.name = name;
         this.birth = birth;
         this.phone = phone;
+        this.gender = gender;
     }
 }

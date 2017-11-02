e6f22bbaaa32daa96d407b4d7cae52a4226c6ff0
dfiialko <denys.fiialko@gmail.com>
2017-11-01 23:14:42 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,16 @@ export class AppComponent {
 
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
+    // By default homepage is 'home page'
     this.page = 'homePage';
   }
 
-
+  // Type is a variabe passed from html page,depending on wihch page user clicks
   changePage(type) {
     this.page = type;
   }
-
+  
+  // Username is passed from html input
   sendUserName(userName) {
     this.username = userName;
   }
